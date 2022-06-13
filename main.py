7cd15cdaf318ae4fce69a41686a429dd8a7f8725
Felipe Néris <104584505+felipevfsbr@users.noreply.github.com>
2022-06-13 06:20:42 -0300

    Inclusão da função Pause

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ from tkinter import *
 from PIL import ImageTk, Image
 import pygame
 from tkinter import filedialog
-#Inicializar mixer
+
+# Inicializar mixer
 pygame.mixer.init()
 
-#Função Adicionar Musica
+
+# Função Adicionar Musica
 def add_musica():
-    musica = filedialog.askopenfilename(initialdir='/audio', title='Escolha uma Musica!', filetypes=(('mp3 Files', '*.mp3'),))
-    #Remover caminho local da musica
+    musica = filedialog.askopenfilename(initialdir='/audio', title='Escolha uma Musica!',
+                                        filetypes=(('mp3 Files', '*.mp3'),))
+    # Remover caminho local da musica
     musica = musica.replace('C:/Users/felip/PycharmProjects/pythonProject1/Audio/', '')
     musica = musica.replace('.mp3', '')
 
-    #Adicionar Musica a lista
+    # Adicionar Musica a lista
     song_box.insert(END, musica)
 
-#Tocar Musica Selecionada
+
+# Tocar Musica Selecionada
 
 def play():
     musica = song_box.get(ACTIVE)
@@ -23,24 +27,44 @@ def play():
     pygame.mixer.music.load(musica)
     pygame.mixer.music.play(loops=0)
 
-#Parar Musica
+
+# Parar Musica
 
 def parar():
     pygame.mixer.music.stop()
+
+
+# Pausa Global
+pausado = False
+
+
+# Pausar Musica
+def pausar(is_pausado):
+    global pausado
+    pausado = is_pausado
+    if pausado:
+        # despausar
+        pygame.mixer.music.unpause()
+        pausado = False
+    else:
+        # pausar
+        pygame.mixer.music.pause()
+        pausado = True
+
 
 root = Tk()
 root.title('Mp3 Player, Made by Felipe Néris')
 root.iconbitmap()
 root.geometry('800x600')
 
-#Inicializando pygame mixer
+# Inicializando pygame mixer
 pygame.mixer.init()
 
-#Playlist
-song_box = Listbox(root, bg='black', fg='yellow', width=60, selectbackground='gray', selectforeground='black')
+# Playlist
+song_box = Listbox(root, bg='blue', fg='yellow', width=60, selectbackground='gray', selectforeground='black')
 song_box.pack(pady=20)
 
-#Botões de controle Imagens
+# Botões de controle Imagens
 
 pausa_img = ImageTk.PhotoImage(Image.open("images/pausa.png"))
 play_img = ImageTk.PhotoImage(Image.open("images/play.png"))
@@ -48,19 +72,18 @@ voltar_img = ImageTk.PhotoImage(Image.open('images/retorna.png'))
 avancar_img = ImageTk.PhotoImage(Image.open('images/avança.png'))
 parar_img = ImageTk.PhotoImage(Image.open('images/stop.png'))
 
-#Botões de controle Moldura
+# Botões de controle Moldura
 
 botoes_moldura = Frame(root)
 botoes_moldura.pack()
 
+# Botões de controle Funções
 
-#Botões de controle Funções
-
-pausa= Button(botoes_moldura, image=pausa_img, borderwidth=0)
-play= Button(botoes_moldura, image=play_img, borderwidth=0, command=play)
-voltar= Button(botoes_moldura, image=voltar_img, borderwidth=0)
-avancar= Button(botoes_moldura, image=avancar_img, borderwidth=0)
-parar= Button(botoes_moldura, image=parar_img, borderwidth=0, command=parar)
+pausa = Button(botoes_moldura, image=pausa_img, borderwidth=0, command=lambda: pausar(pausado))
+play = Button(botoes_moldura, image=play_img, borderwidth=0, command=play)
+voltar = Button(botoes_moldura, image=voltar_img, borderwidth=0)
+avancar = Button(botoes_moldura, image=avancar_img, borderwidth=0)
+parar = Button(botoes_moldura, image=parar_img, borderwidth=0, command=parar)
 
 pausa.grid(row=0, column=2, padx=10)
 play.grid(row=0, column=3, padx=10)
@@ -68,17 +91,15 @@ voltar.grid(row=0, column=0, padx=10)
 avancar.grid(row=0, column=1, padx=10)
 parar.grid(row=0, column=4, padx=10)
 
-#Criar Menu
+# Criar Menu
 
 meu_menu = Menu(root)
 root.config(menu=meu_menu)
 
-#Menu para Adicionar Música
+# Menu para Adicionar Música
 
 menu_add_musica = Menu(meu_menu)
 meu_menu.add_cascade(label='Adicionar Musicas', menu=menu_add_musica)
 menu_add_musica.add_command(label='Adicionar uma Música á Playlist.', command=add_musica)
 
 root.mainloop()
-
-
